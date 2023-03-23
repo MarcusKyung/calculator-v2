1d2a73e09ccca816cf5fd9474a83fdb9dc1b3f88
MarcusKyung <Kyungmj@gmail.com>
2023-03-23 13:18:17 -0700

    fix JS calculator functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,58 @@
 //Business Logic
 
 function add(number1, number2) {
-  result = number1 + number2;
+  return number1 + number2;
 }
 
 function subtract(number1, number2) {
-  result = number1 - number2;
+  return number1 - number2;
 }
 
 function multiply(number1, number2) {
-  result = number1 * number2;
+  return number1 * number2;
 }
 
 function divide(number1, number2) {
-  result = number1 / number2;
+  return number1 / number2;
 }
 
 
 //User Logic
+
 function operator(event) {
   event.preventDefault();
   const number1 = parseInt(document.querySelector("input#number1").value);
   const number2 = parseInt(document.querySelector("input#number2").value);
   const op = document.querySelector("input[name='operator']:checked").value;
 
-if (op === "add") {
-  answer = add(number1, number2);
+  let result;
+  if (op === "add") {
+    result = add(number1, number2);
   } else if (op === "subtract") {
-    answer = subtract(number1, number2);
+    result = subtract(number1, number2);
   } else if (op === "multiply") {
-    answer = multiply(number1, number2);
+    result = multiply(number1, number2);
   } else if (op === "divide") {
-    answer = divide(number1, number2);
+    result = divide(number1, number2);
   }
 
-window.addEventListener("submit", function() {
-  document.getElementById("p").innerText = answer;
-});
-
-window.addEventListener("load", function() {
-  document.getElementById("calculator").addEventListener("submit", operator);
-  event.preventDefault();
-});
+  document.getElementById("output").innerText = result;
 }
+
+  window.addEventListener("load" ,function() {
+    const form= document.getElementById("calculator");
+    form.addEventListener("submit", operator);
+  });
+
+
+
+
+
+// window.addEventListener("submit", function() {
+//   document.getElementById("p").innerText = answer;
+//   event.preventDefault();
+// });
+
+// window.addEventListener("load", function() {
+//   document.getElementById("calculator").addEventListener("submit", operator());
+// });
